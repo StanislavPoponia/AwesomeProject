ed59de5d43e851b689b04d541c1a8a0fda70e003
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-10-15 17:50:10 +0300

    homework 2/screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screen/AuthPages/LoginScreen/LoginScreen';
+import RegistrationScreen from './screen/AuthPages/RegistrationScreen/RegistrationScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
